8cdbd08c451f0a57f8fb928571c5387d2fceab8e
youngrivers <401712228@qq.com>
2016-08-08 23:23:46 +0800

    43

---
+++ Task43/script.js
@@ -0,0 +1,15 @@
+/**
+ * Created by youngrivers on 2016/8/8.
+ */
+(function () {
+    var choose=document.getElementById("choose");
+    var frame=document.getElementById("frame");
+    var c=choose.value;
+    //frame.style.background-color="#96254";
+    console.log(c);
+    /*switch (1){
+        case (c==1)
+            break;
+
+    }*/
+})();
