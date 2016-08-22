14a7ac48a21cf6a7d5c46d303c4518aca30e3dfe
youngrivers <401712228@qq.com>
2016-08-22 10:42:24 +0800

    点击浮出层

--- Task37/script.js
+++ Task37/script.js
@@ -2,14 +2,17 @@
  * Created by youngrivers on 2016/5/28.
  */
 var login=document.getElementById("login");
-var c2=document.getElementById("c2");
 var loginBox=document.getElementById("login-box");
 var closeBox=document.getElementById("close-box");
 login.onclick=function(){
-    c2.style.backgroundColor="#ccc";
     loginBox.style.display="block";
+    document.getElementsByClassName("flex-box")[0].style.visibility="visible"
 };
 closeBox.onclick= function () {
-    c2.style.backgroundColor="#fff";
     loginBox.style.display="none";
+    document.getElementsByClassName("flex-box")[0].style.visibility="hidden"
+};
+document.getElementsByClassName("flex-box")[0].onclick=function () {
+    loginBox.style.display="none";
+    document.getElementsByClassName("flex-box")[0].style.visibility="hidden"
 };
